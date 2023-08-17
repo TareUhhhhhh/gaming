014ee52fdf49b700434090b21d5d7a0b97637c26
Tara <tara.haskamp@gmail.com>
2023-08-17 15:14:56 -0500

    Add page and post type to index

--- wp-content/themes/gaming2023/index.php
+++ wp-content/themes/gaming2023/index.php
@@ -1,5 +1,23 @@
 <?php
 get_header();
+
+if (is_page('home')) {
+		include('pages/home.php');
+	}
+
+if (is_page('games')) {
+	 $args = array(  
+	     'post_type' => 'game',
+	 );
+
+	 $loop = new WP_Query( $args ); 
+	     
+	 while ( $loop->have_posts() ) : $loop->the_post(); 
+	     include('pages/game-detail.php');
+	 endwhile;
+
+	 wp_reset_postdata(); 
+}
 // Silence is golden.
 get_footer();
 ?>
