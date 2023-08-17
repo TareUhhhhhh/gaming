7bcfe95768d52379424e83f4bd5c76d88e28f6db
Tara <tara.haskamp@gmail.com>
2023-08-17 18:58:24 -0500

    Add game detail information

--- wp-content/themes/gaming2023/pages/game-detail.php
+++ wp-content/themes/gaming2023/pages/game-detail.php
@@ -1,1 +1,9 @@
-game-detail.php
+<section class="game-detail">
+	<h2 class='game_name'><?php the_field('game_name') ?></h2>
+	<picture class="game_image">
+		<img src="<?php the_field('game_image') ?>" alt="">
+	</picture>
+	<p class="esrb_rating"><span class="label">ESRB Rating: </span><?php the_field('esrb_rating') ?></p>
+	<p class="genre"><span class="label">Genre: </span> <?php the_field('genre')?></p>
+	<p class="game_description"><?php the_field('game_description')?></p>
+</section>
