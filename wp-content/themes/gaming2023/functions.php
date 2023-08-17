0a89e368af1f800ab032808b0ca35316a4dec4e1
Tara <tara.haskamp@gmail.com>
2023-08-17 15:14:28 -0500

    Add style sheet and menu functions

--- wp-content/themes/gaming2023/functions.php
+++ wp-content/themes/gaming2023/functions.php
@@ -1,3 +1,14 @@
+<?php
+
+add_filter('show_admin_bar', '__return_false');
+
+function mytheme_enqueue_style() {
+	wp_enqueue_style ('mytheme-style', get_stylesheet_uri());
+}
+add_action ('wp_enqueue_scripts', 'mytheme_enqueue_style');
+
+wp_enqueue_style( 'style', get_stylesheet_uri() );
+
 function register_my_menu() {
 	register_nav_menu('header-menu',__( 'Header Menu'));
 }
