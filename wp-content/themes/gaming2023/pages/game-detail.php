f7f50755c2c3b7d301e7f7da2037d21d8e4add65
Tara <tara.haskamp@gmail.com>
2023-08-17 15:13:35 -0500

    Add pages folder and game detail plus home files

---
+++ wp-content/themes/gaming2023/pages/game-detail.php
@@ -0,0 +1,6 @@
+<section class="artist-detail">
+	<h2 class="game_name"><?php the_field('game_name') ?></h2>
+	<picture class="esrb_rating"><img src="<?php the_field('esrb_rating') ?>" alt=""></picture>
+	<p class="genre"><?php the_field('genre')?></p>
+	<p class="game_teaser"><?php the_field('game_teaser')?></p>
+</section>
