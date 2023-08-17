2a25a9ad769be5499b0508ec544aed42bc7da1f8
Tara <tara.haskamp@gmail.com>
2023-08-17 18:56:50 -0500

    Add link to game detail page

--- wp-content/themes/gaming2023/pages/game-card.php
+++ wp-content/themes/gaming2023/pages/game-card.php
@@ -4,6 +4,7 @@
 		<img src="<?php the_field('game_image') ?>" alt="">
 	</picture>
 	<p class="game_teaser"><?php the_field('game_teaser')?></p>
-		<p class="esrb_rating"><span class="label">ESRB Rating: </span><?php the_field('esrb_rating') ?></p>
+	<p class="esrb_rating"><span class="label">ESRB Rating: </span><?php the_field('esrb_rating') ?></p>
 	<p class="genre"><span class="label">Genre: </span> <?php the_field('genre')?></p>
+	<a href="<?php the_permalink(); ?>">More info</a>
 </section>
