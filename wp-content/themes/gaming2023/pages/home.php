3f61489a0be7e2576d21e64b69acc1fcda6e6f37
Tara <tara.haskamp@gmail.com>
2023-08-17 22:58:21 -0500

    Add update section to home page

--- wp-content/themes/gaming2023/pages/home.php
+++ wp-content/themes/gaming2023/pages/home.php
@@ -3,3 +3,16 @@
 	<h3 class="intro">A Quick Wordpress Workup</h3>
 	<p class="page-description">This is a page created to practice some Wordpress goodness.</p>
 </section>
+
+<?php
+ $args = array(  
+     'post_type' => 'updates',
+ );
+
+ $loop = new WP_Query( $args ); 
+     
+ while ( $loop->have_posts() ) : $loop->the_post(); 
+     include('update-card.php');
+ endwhile;
+
+ wp_reset_postdata(); 
