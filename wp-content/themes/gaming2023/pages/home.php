c1b90bc3e3b72609a1247e85e8525cf3edc31e62
Tara <tara.haskamp@gmail.com>
2023-08-17 19:09:46 -0500

    Add homepage content

---
+++ wp-content/themes/gaming2023/pages/home.php
@@ -0,0 +1,6 @@
+
+<section class="home-intro">
+	<h1 class='title'>Turra Gaming</h1>
+	<h3 class='intro'>A Quick Wordpress Workup</h3>
+	<p class="page-description">This is a page created to practice some Wordpress goodness.</p>
+</section>
