f39ee7bd6e43634c4bd42157b56e3829757e7a30
Tara <tara.haskamp@gmail.com>
2023-08-17 20:34:41 -0500

    Correct syntax inconsistencies

--- wp-content/themes/gaming2023/pages/game-card.php
+++ wp-content/themes/gaming2023/pages/game-card.php
@@ -1,10 +1,10 @@
 <section class="game-card">
-	<h2 class="game_name"><?php the_field('game_name') ?></h2>
-	<picture class="game_image">
+	<h2 class="game-name"><?php the_field('game_name') ?></h2>
+	<picture class="game-image">
 		<img src="<?php the_field('game_image') ?>" alt="">
 	</picture>
-	<p class="game_teaser"><?php the_field('game_teaser')?></p>
-	<p class="esrb_rating"><span class="label">ESRB Rating: </span><?php the_field('esrb_rating') ?></p>
+	<p class="game-teaser"><?php the_field('game_teaser')?></p>
+	<p class="esrb-rating"><span class="label">ESRB Rating: </span><?php the_field('esrb_rating') ?></p>
 	<p class="genre"><span class="label">Genre: </span> <?php the_field('genre')?></p>
 	<a href="<?php the_permalink(); ?>">More info</a>
 </section>
--- wp-content/themes/gaming2023/pages/game-detail.php
+++ wp-content/themes/gaming2023/pages/game-detail.php
@@ -1,9 +1,9 @@
 <section class="game-detail">
-	<h2 class='game_name'><?php the_field('game_name') ?></h2>
-	<picture class="game_image">
+	<h2 class="game-name"><?php the_field('game_name') ?></h2>
+	<picture class="game-image">
 		<img src="<?php the_field('game_image') ?>" alt="">
 	</picture>
-	<p class="esrb_rating"><span class="label">ESRB Rating: </span><?php the_field('esrb_rating') ?></p>
+	<p class="esrb-rating"><span class="label">ESRB Rating: </span><?php the_field('esrb_rating') ?></p>
 	<p class="genre"><span class="label">Genre: </span> <?php the_field('genre')?></p>
-	<p class="game_description"><?php the_field('game_description')?></p>
+	<p class="game-description"><?php the_field('game_description')?></p>
 </section>
