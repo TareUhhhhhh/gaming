8ffa0441f59f0aa4a8ee02021a1986d2cff25fa6
Tara <tara.haskamp@gmail.com>
2023-08-18 13:01:31 -0500

    Add update detail content

---
+++ wp-content/themes/gaming2023/pages/update-detail.php
@@ -0,0 +1,8 @@
+<section class="update">
+	<p class="date"><?php the_field('date')?></p>
+	<h3 class="headline"><?php the_field('headline') ?></h2>
+	<picture class="image">
+		<img src="<?php the_field('image') ?>" alt="">
+	</picture>
+	<p class="update-content"><?php the_field('update_content')?></p>
+</section>
